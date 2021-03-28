9bd827dd6ca91cb67a05170ad08fd7ddfe3b8c41
afulwider <aa.fulwider@gmail.com>
2021-03-27 21:14:11 -0700

    made notes

---
+++ Assets/Notes.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+/* 
+ * Game will be battleground wave based. Each time a wave is completed, the player will be given a mushroom if he
+ * is small and either a feather or a flower if he is tall and a number of special items if he already has one of those.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *  
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
